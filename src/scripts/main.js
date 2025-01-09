6eefd98dab9510991e9ca5bb63138e52d2b24528
Irohosi Pixel <irohosi-pixel@ihpx.net>
2025-01-09 18:48:53 +0900

    fix: トップへ戻るボタンが表示されない問題を修正

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,19 @@ import 'https://code.jquery.com/jquery-3.7.1.min.js';
 
 $(function () {
   let lastScrollTop = 0;
-  const header = $('header');
-  const topBtn = $('.page-top');
+  const $header = $('header');
+  const $topBtn = $('.page-top');
   $(window).scroll(function () {
-    $(this).scrollTop() > 150 ? topBtn.fadeIn() : topBtn.fadeOut();
+    $(this).scrollTop() > 150 ? $topBtn.fadeIn() : $topBtn.fadeOut();
     let currentScrollTop = $(this).scrollTop();
     if (currentScrollTop > lastScrollTop) {
-      header.css('transform', 'translateY(-120%)');
+      $header.css('transform', 'translateY(-120%)');
     } else {
-      header.css('transform', 'translateY(0)');
+      $header.css('transform', 'translateY(0)');
     }
     lastScrollTop = currentScrollTop;
   });
-  topBtn.click(function () {
+  $topBtn.click(function () {
     $('body,html').animate({ scrollTop: 0 }, 500);
     return false;
   });
